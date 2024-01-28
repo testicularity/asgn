da6aec1f42d6ebf1f6dc958b44257849aa1b45b6
testicularity <156512864+testicularity@users.noreply.github.com>
2024-01-28 16:32:21 +0800

    Update h1.h

--- h1.h
+++ h1.h
@@ -1,52 +1,68 @@
 #pragma once
 
+using namespace std;
+
 
 class Memory {
-    int data[64] = {};
+    unsigned char data[64] = {};
     public:
-        const int& MAX_SIZE = data.size();
-        void print();
+        const int MAX_SIZE = 64;
+        void print(ofstream& ofs);
         void writeVal(const int& val , const int& addr);
         int getVal(const int& addr);
         void clear(const int& addr);
 
-        Memory() {}
+        Memory() {
+            for(int i = 0; i < MAX_SIZE; i++) 
+                data[i] = 0; 
+        }
     
 };
 
 class Regs {
-    int data[7] = {};
+    unsigned char data[7] = {};
     public:
-        const int& MAX_SIZE = data.size();
-        void print();
+        const int MAX_SIZE = 7;
+        void print(ofstream& ofs);
         void writeVal(const int& val , const string& reg);
         int getVal(const string& reg);
         void clear(const string& reg);
 
-        Regs() {}
+        Regs() {
+            for(int i = 0; i < MAX_SIZE; i++) 
+                data[i] = 0; 
+        }
 };
 
 class Flags {
-    bool data[2] = {}; //{zero, carry}
+    bool data[4] = {}; //{zero, carry , of , uf}
     public:
-        const int& ZF = 0;
-        const int& CF = 1; 
-        void print();
+        const int MAX_SIZE = 4;
+        const int ZF = 0;
+        const int CF = 1; 
+        const int OF = 2; 
+        const int UF = 3; 
+        void print(ofstream& ofs);
         void set(const int& flag , const bool& val);
         bool get(const int& addr);
 
-        Flags() {}
+        Flags() {
+            for(int i = 0; i < MAX_SIZE; i++) 
+                data[i] = 0; 
+        }
 };
 
 void runner(); //openfile etc
 void operate(const string&); //operate a line, call func based on first word
 
 //helper functions:
-vector<string> split(const string& , const char&); //split a string into words, then return vector of words
+bool isValid(const char&);
+char lowerCase(const char&);
+vector<string> split(const string&); //split a string into words, then return vector of words
 int readFrom(const string&);
 void writeTo(const string& , const int&);
-int toAByte(int const& r);
-void conversion (int n);
+int* toBinary(int n);
+int toInt(int* arr);
 
 
 
@@ -55,8 +71,11 @@ void in(vector<string>& words);
 void out(vector<string>& words);
 void mov(vector<string>& words);
 void arth(const char& c , vector<string>& words);
-void shift(const char& c , vector<string>& words)
+void shift(const char& c , vector<string>& words);
 void rotate(const char& c , vector<string>& words);
 void load(vector<string>& words);
 void store(vector<string>& words);
+
+//other functions:
+void errorHandler(const string&);
 
