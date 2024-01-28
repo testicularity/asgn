1950e3669b8b6fb24c523933984d0d2ff1048883
testicularity <156512864+testicularity@users.noreply.github.com>
2024-01-28 16:31:34 +0800

    Update v1.cpp

--- v1.cpp
+++ v1.cpp
@@ -1,6 +1,11 @@
-#include "hi.h"
+
+#include <iomanip>
 #include <iostream>
+#include <fstream>
+#include <sstream>
 #include <vector>
+#include <string>
+#include "h1.h"
 
 using namespace std;
 
@@ -8,24 +13,65 @@ using namespace std;
 Memory mem;
 Regs reg;
 Flags flag;
+int pc = 0;
 
 
 
 void operate(const string& s) { //check for number of operands
-    vector<string> words = split(s , ' ');
-    if(words == "IN") {
-        in
+    vector<string> words = split(s);
+    if(s == "") return;
+    pc++;
+    if(words[0] == "in") {
+        if(words.size() == 2)
+            in(words);
+    }
+    else if(words[0] == "out") {
+        if(words.size() == 2)
+            out(words);
+    }
+    else if(words[0] == "mov") {
+        if(words.size() == 3){
+            mov(words);
+            
+        }
+    }
+    else if(words[0] == "add") {
+        arth( '+' , words);
+    }
+    else if(words[0] == "sub") {
+        arth( '-' , words);
+    }
+    else if(words[0] == "mul") {
+        arth( '*' , words);
+    }
+    else if(words[0] == "div") {
+        arth( '/' , words);
+    }
+    else if(words[0] == "store") {
+        store(words);
+    }
+    else if(words[0] == "load") {
+        load(words);
+    }
+    else {//operation not found
+
+
     }
 }
 
 void runner() { //open files etc
-    openfile();
-    ifstream fs;
-    fs.open("filename");
+    ifstream ifs {"/home/a/test/cppfiles/x/input.txt"};
     string s;
-    while(getline(fs , s)) {
+    while(getline(ifs , s)) {
         operate(s); //s = ADD R1, R6
     }
+    ofstream ofs {"/home/a/test/cppfiles/x/output.txt"};
+    
+    reg.print(ofs);
+    flag.print(ofs);
+    ofs << setw(10) << left << "PC" << ": " << pc << "\n\n";
+    mem.print(ofs);
+    ofs.close();
 }
 
 
@@ -33,21 +79,8 @@ void runner() { //open files etc
 
 int main() { //to specify input/output file, can add options as well like verbose mode...
     
-    string line; //Pc
-    string s;
-    stringstream ss;
+    runner();
 
-
-    while(getline(cin , line , '\n')) {
-        ss.str(line);
-        ss >> s; //first word of the line
-        if(!fc.contains(s)) {
-            //operator not found (prints the error line and reg and table val b4 encountering the error)
-            return 1;
-        }
-
-
-
-    }
+    return 0;
     
 }    
